00bb9c48e3890f3047df561636834a331fdb9c32
shoshi <shoshiroberts@gmail.com>
2019-01-14 15:40:20 -0500

    Better social button text display

--- components/SocialShareButtons.vue
+++ components/SocialShareButtons.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex-grid sml-flex-row text-center">
     <ShareButton
-      network="twitter"
+      network="Twitter"
       :url="url"
       :should-display-icon="false"
       class="btn-sml btn-block"
       @click.native="$trackClick('twitter_share_button')" />
     <ShareButton
-      network="facebook"
+      network="Facebook"
       :url="url"
       :should-display-icon="false"
       class="btn-sml btn-block"
