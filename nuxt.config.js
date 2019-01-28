130fd220bb904763a620cf715d1b0c68037ea0c9
shoshi <shoshiroberts@gmail.com>
2019-01-28 17:58:39 -0500

    Update router scroll behavior

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,8 +27,21 @@ module.exports = {
   ** Router configuration
   */
   router: {
-    scrollBehavior: function (to, from, savedPosition) {
-      return { x: 0, y: 0 }
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        let position = {}
+        if (to.matched.length < 2) {
+          position = { x: 0, y: 0 }
+        } else if (to.matched.some(r => r.components.default.options.scrollToTop)) {
+          position = { x: 0, y: 0 }
+        }
+        if (to.hash) {
+          position = { selector: to.hash }
+        }
+        return position
+      }
     }
   },
 
