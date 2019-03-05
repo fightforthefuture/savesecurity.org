c87e2ab0f460669ca7cf06656afaf32cd5df1601
shoshi <shoshiroberts@gmail.com>
2019-03-05 17:22:33 -0500

    Proof of concept - Styleguide

---
+++ components/StyleguideLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <header class="page-header">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c6 med-c8">
+            <nuxt-link to="/styleguide">
+              <h2>{{ title }} - Styleguide</h2>
+            </nuxt-link>
+          </div> <!-- .c -->
+          <div class="sml-c6 med-c4">
+            <ul class="hoz sml-push-y-half">
+              <li><nuxt-link to="/styleguide/grid">Grid</nuxt-link></li>
+              <li><nuxt-link to="/styleguide/typography">Typography</nuxt-link></li>
+              <li><nuxt-link to="/styleguide/ActionNetworkForm">ActionNetworkForm</nuxt-link></li>
+            </ul> <!-- .flex-grid -->
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </header>
+
+    <div class="site-content">
+      <section class="sml-pad-y5">
+        <div class="wrapper">
+          <slot />
+        </div> <!-- .wrapper -->
+      </section>
+    </div> <!-- .site-content -->
+  </div>
+</template>
+
+<script>
+import config from '~/config'
+
+export default {
+  computed: {
+    title() { return config.siteTitle }
+  }
+}
+</script>
---
+++ pages/styleguide/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <StyleguideLayout>
+    <h1>{{ title }}</h1>
+    <component :is="componentInstance" class="sml-push-y2 med-push-y4"></component>
+  </StyleguideLayout>
+</template>
+
+<script>
+import StyleguideLayout from '~/components/StyleguideLayout'
+
+export default {
+  components: {
+    StyleguideLayout
+  },
+
+  head() {
+    return {
+      titleTemplate: `%s - Styleguide - ${this.id}`
+    }
+  },
+
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+    title() {
+      return `${this.id.charAt(0).toUpperCase()}${this.id.slice(1)}`
+    },
+    componentInstance() {
+      return () => import(`~/components/${this.id}`)
+    }
+  }
+}
+</script>
