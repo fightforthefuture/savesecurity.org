cc4ef35509b83ee36d8ee42b09b802b93f618b0b
shoshi <shoshiroberts@gmail.com>
2019-03-05 18:01:11 -0500

    Test for classes

--- components/typography.vue
+++ components/typography.vue
@@ -6,5 +6,34 @@
     <h4>Heading 4</h4>
     <h5>Heading 5</h5>
     <h6>Heading 6</h6>
+
+    <p v-for="c in classes"
+       :key="`type-class-${c}`"
+       class="sml-push-y2"
+       :class="c">
+      This is some text <strong>in bold</strong> and <em>emphasized</em>,
+      <a href="#">with a link</a>.
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      classes: [
+        '',
+        'text-loud',
+        'text-center',
+        'text-right',
+        'text-left',
+        'text-white',
+        'text-meta',
+        'text-brand',
+        'text-warn',
+        'text-success'
+      ]
+    }
+  }
+}
+</script>
