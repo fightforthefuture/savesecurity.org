e9fbe2b7a0b0c781917afe28be41d01a4cbd47cc
shoshi <shoshiroberts@gmail.com>
2019-07-09 14:01:28 -0400

    No-JS friendly nav toggle

--- components/StyleguideLayout.vue
+++ components/StyleguideLayout.vue
@@ -19,87 +19,139 @@
           &#9776;
           <span class="text-accessibility">Menu</span>
         </label>
-        <input type="checkbox" id="menu-toggle" />
+        <input type="checkbox" id="menu-toggle" v-model="checked" />
         <div id="menu" class="sml-pad-2">
-          <h2><nuxt-link to="/styleguide/base">Base</nuxt-link></h2>
+          <h2>
+            <nuxt-link to="/styleguide/base" @click.native="toggleNav">
+              Base
+            </nuxt-link>
+          </h2>
           <ul>
             <li>
-              <nuxt-link to="/styleguide/base#colors">Colors</nuxt-link>
+              <nuxt-link to="/styleguide/base#colors" @click.native="toggleNav">
+                Colors
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#grid">Grid</nuxt-link>
+              <nuxt-link to="/styleguide/base#grid" @click.native="toggleNav">
+                Grid
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#headings">Headings</nuxt-link>
+              <nuxt-link to="/styleguide/base#headings" @click.native="toggleNav">
+                Headings
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#typography">Typography</nuxt-link>
+              <nuxt-link to="/styleguide/base#typography" @click.native="toggleNav">
+                Typography
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#spacing">Spacing</nuxt-link>
+              <nuxt-link to="/styleguide/base#spacing" @click.native="toggleNav">
+                Spacing
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#modifiers">Modifiers</nuxt-link>
+              <nuxt-link to="/styleguide/base#modifiers" @click.native="toggleNav">
+                Modifiers
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#buttons">Buttons</nuxt-link>
+              <nuxt-link to="/styleguide/base#buttons" @click.native="toggleNav">
+                Buttons
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#forms">Forms</nuxt-link>
+              <nuxt-link to="/styleguide/base#forms" @click.native="toggleNav">
+                Forms
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/base#lists">Lists</nuxt-link>
+              <nuxt-link to="/styleguide/base#lists" @click.native="toggleNav">
+                Lists
+              </nuxt-link>
             </li>
           </ul>
           <h2 class="sml-push-y2">Components</h2>
           <ul>
             <li>
-              <nuxt-link to="/styleguide/ActionNetworkForm">ActionNetworkForm</nuxt-link>
+              <nuxt-link to="/styleguide/ActionNetworkForm" @click.native="toggleNav">
+                ActionNetworkForm
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/CallForm">CallForm</nuxt-link>
+              <nuxt-link to="/styleguide/CallForm" @click.native="toggleNav">
+                CallForm
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/Logo">Logo</nuxt-link>
+              <nuxt-link to="/styleguide/Logo" @click.native="toggleNav">
+                Logo
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/SocialShareButtons">SocialShareButtons</nuxt-link>
+              <nuxt-link to="/styleguide/SocialShareButtons" @click.native="toggleNav">
+                SocialShareButtons
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/QuoteScroller">QuoteScroller</nuxt-link>
+              <nuxt-link to="/styleguide/QuoteScroller" @click.native="toggleNav">
+                QuoteScroller
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/LogoCloud">LogoCloud</nuxt-link>
+              <nuxt-link to="/styleguide/LogoCloud" @click.native="toggleNav">
+                LogoCloud
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/Map">Map</nuxt-link>
+              <nuxt-link to="/styleguide/Map" @click.native="toggleNav">
+                Map
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/ClickToCopy">ClickToCopy</nuxt-link>
+              <nuxt-link to="/styleguide/ClickToCopy" @click.native="toggleNav">
+                ClickToCopy
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/Scoreboard">Scoreboard</nuxt-link>
+              <nuxt-link to="/styleguide/Scoreboard" @click.native="toggleNav">
+                Scoreboard
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/SelfieForm">SelfieForm</nuxt-link>
+              <nuxt-link to="/styleguide/SelfieForm" @click.native="toggleNav">
+                SelfieForm
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/ReadTheLetter">ReadTheLetter</nuxt-link>
+              <nuxt-link to="/styleguide/ReadTheLetter" @click.native="toggleNav">
+                ReadTheLetter
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/PrintTheLetter">PrintTheLetter</nuxt-link>
+              <nuxt-link to="/styleguide/PrintTheLetter" @click.native="toggleNav">
+                PrintTheLetter
+              </nuxt-link>
             </li>
           </ul>
           <h2 class="sml-push-y2">Modals</h2>
           <ul>
             <li>
-              <nuxt-link to="/styleguide/ArchivedModal">ArchivedModal</nuxt-link>
+              <nuxt-link to="/styleguide/ArchivedModal" @click.native="toggleNav">
+                ArchivedModal
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/CallFormModal">CallFormModal</nuxt-link>
+              <nuxt-link to="/styleguide/CallFormModal" @click.native="toggleNav">
+                CallFormModal
+              </nuxt-link>
             </li>
             <li>
-              <nuxt-link to="/styleguide/CallScriptModal">CallScriptModal</nuxt-link>
+              <nuxt-link to="/styleguide/CallScriptModal" @click.native="toggleNav">
+                CallScriptModal
+              </nuxt-link>
             </li>
           </ul>
         </div> <!-- #menu -->
@@ -136,9 +188,24 @@ export default {
     }
   },
 
+  data() {
+    return {
+      checked: false
+    }
+  },
+
   computed: {
     title() { return config.siteTitle },
     pageTitle() { return this.componentName ? `- ${this.componentName}` : '' }
+  },
+
+  methods: {
+    toggleNav() {
+      // NOTE: This respects both the No-JS version where CSS controls the menu
+      //       display, and the JS version where clicking on the *currently
+      //       active* link needs to close the nav.
+      this.checked = !this.checked
+    }
   }
 }
 </script>
